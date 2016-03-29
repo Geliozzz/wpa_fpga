d5a6def07cf4d12248cce368f89bf981114a5394
Geliozzz <o.pocheptsov@gmail.com>
2016-03-29 22:04:32 +0300

    ram порт подключен в файле верхнего уровня.
    Более большие приложения начали компилироваться.

--- wpa.v
+++ wpa.v
@@ -1,7 +1,18 @@
 module wpa
 (
 	input clk,
-	input reset
+	input reset,
+	output [ 12: 0] sdram_addr,
+	output [  1: 0] sdram_ba,
+	output 			 sdram_cas_n,
+	output			 sdram_cke,
+	output			 sdram_cs_n,
+	inout	 [ 15: 0] sdram_dq,
+	output [  1: 0] sdram_dqm,
+	output			 sdram_ras_n,
+	output			 sdram_we_n,
+	output			 sdram_clk
+	
 );
 
 wire [31 : 0] write;
@@ -16,7 +27,17 @@ wpa2 nios(
 				 .read_export(read),
 				 .control_export(control),
 				 .address_export(address),
-				 .reset_reset_n(reset)
+				 .reset_reset_n(reset),
+				 
+				 .ram_wire_addr(sdram_addr),
+				 .ram_wire_ba(sdram_ba),
+				 .ram_wire_cas_n(sdram_cas_n),
+				 .ram_wire_cke(sdram_cke),
+				 .ram_wire_cs_n(sdram_cs_n),
+				 .ram_wire_dq(sdram_dq),
+				 .ram_wire_dqm(sdram_dqm),
+				 .ram_wire_ras_n(sdram_ras_n),
+				 .ram_wire_we_n(sdram_we_n)
 				);
 				
 sha1 sha(
@@ -29,6 +50,7 @@ sha1 sha(
 				 .we(control[1]),
 				 .error(error)
 				);
+			
 				
 //assign control[2] = error;
 				
